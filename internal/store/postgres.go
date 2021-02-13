aa831a9b3e2c1c303dde369f2189a7b01fa2016f
Monta Gao <mgao@splunk.com>
2021-02-13 12:50:31 -0800

    bring defers back

--- internal/store/postgres.go
+++ internal/store/postgres.go
@@ -146,7 +146,7 @@ func (s *PlanStore) Delete(id int64) error {
 func (s *PlanStore) updateList1(plan *models.Plan) error {
 	stmt, err := s.db.Prepare("INSERT INTO plans(id, list1, name1, timestamp, is_complete) VALUES( $1, $2, $3, $4, $5 );")
 	// Prepared statements take up server resources and should be closed after use.
-	// defer stmt.Close()
+	defer stmt.Close()
 	fmt.Printf("got plan %v", plan)
 
 	if err != nil {
@@ -159,6 +159,7 @@ func (s *PlanStore) updateList1(plan *models.Plan) error {
 func (s *PlanStore) updateList2(plan *models.Plan) error {
 	stmt, err := s.db.Prepare("UPDATE plans SET list2 = $1, name2 = $2, timestamp = $3, is_complete = $4  WHERE id = $5;")
 	// Prepared statements take up server resources and should be closed after use.
+	defer stmt.Close()
 	if err != nil {
 		return err
 	}
